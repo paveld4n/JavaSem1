e8be518734b673775f76fd2066efc5673ed58a6a
Pavel <rut100500@yandex.ru>
2023-06-08 18:30:34 +0300

    Сем 1 вариант 4

--- Main.java
+++ Main.java
@@ -2,22 +2,21 @@ public class Main {
     
     public static void main (String[] args) {
         
-        Triangle t1 = new Triangle();
-        Triangle t2 = new Triangle();
+        Triangle t1 = new Triangle(4.0, 2.0, "color RED");
+        Triangle t2 = new Triangle(3, 12, "color BLUE");
 
-        t1.setHeight(4); 
-        t1.setWight(2);
-        t1.style ="filled";
+        //t1.setHeight(4); 
+        //t1.setWight(2);
+        //t1.style ="filled";
         t1.show_shape();
         t1.show_style();
 
-        t2.setHeight(3);
-        t2.setWight(2);
-        t2.style = "Empty";
+        //t2.setHeight(3);
+        //t2.setWight(2);
+        //t2.style = "Empty";
         t2.show_shape();
         t2.show_style();
 
     }
-
-    
+   
 }
--- Triangle.java
+++ Triangle.java
@@ -6,6 +6,19 @@ public class Triangle extends Shape {
         return getWight() * getHeight() / 2;
     }
     void show_style(){
-        System.out.println(style);
+        System.out.println("Форма =" + style);
+    }
+    @Override
+    void show_shape() {
+        System.out.println("Гипотенуза =" + Math.sqrt(getWight()*getWight()
+        +getHeight()*getHeight()));
+        // Переопределение позволяет подклассу по своему
+        // использовать метод родителя
+    }
+    public Triangle( double wight, double height, String style) {
+        // задаем кастомный конструктор
+        setWight(wight);
+        setHeight(height);
+        this.style = style;
     }
 }
